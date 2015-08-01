cedaba6624a06ed90d59c5c75cc2d7dfdbe1800d
EMederos <emederos@hotmail.com>
2015-08-01 17:52:27 +0100

    Editar preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,13 +18,13 @@ exports.index = function(req,res) {
     models.Quiz.findAll({where: ["pregunta like ?","%"+swhere+"%"],
                          order: [["pregunta","ASC"]]})
         .then(function(quizes) {
-            res.render('quizes/index.ejs', { quizes:quizes });
+            res.render('quizes/index.ejs', { quizes:quizes, errors:[] });
     }).catch(function(error) { next(error);});
 };
 
 // GET /quizes/:id
 exports.show=function(req,res) {
-    res.render('quizes/show', {quiz: req.quiz});
+    res.render('quizes/show', {quiz: req.quiz, errors:[] });
 };
 
 // GET /quizes/question
@@ -41,7 +41,7 @@ exports.answer = function(req, res) {
         resultado = 'Correcto';
     }
     res.render('quizes/answer', { quiz: req.quiz, 
-                                  respuesta: resultado });
+                                  respuesta: resultado, errors:[] });
 };
 
 // GET /quizes/new
@@ -49,16 +49,45 @@ exports.new = function(req, res) {
     var quiz = models.Quiz.build( //crea objeto quiz
         {pregunta: "Pregunta", respuesta: "Respuesta" }
     );
-    res.render('quizes/new', { quiz: quiz });
+    res.render('quizes/new', { quiz: quiz, errors:[] });
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
     var quiz = models.Quiz.build( req.body.quiz );
 
-    // guarda en BD los campos
-    quiz.save({fields:["pregunta","respuesta"]}).then(function() {
-        res.redirect('/quizes');
+    quiz.validate().then( function(err) {
+        if (err) {
+            res.render('quizes/new', {quiz: quiz, errors: err.errors});
+        } else {
+            // guarda en BD los campos
+            quiz.save({fields:["pregunta","respuesta"]}).then(function() {
+                res.redirect('/quizes');
+            });
+        }
     });
 };
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+    var quiz = req.quiz;
+    res.render('quizes/edit', { quiz: quiz, errors:[] });
+};
+
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+
+    req.quiz.validate().then( function(err) {
+        if (err) {
+            res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+        } else {
+            // guarda en BD los campos
+            req.quiz.save({fields:["pregunta","respuesta"]}).then(function() {
+                res.redirect('/quizes');
+            });
+        }
+    });
+};
