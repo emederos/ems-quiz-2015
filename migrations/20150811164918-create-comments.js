3c46cae2ec76418adc6801c8042098211daf440b
EMederos <emederos@hotmail.com>
2015-08-11 17:01:09 +0100

    Creación de tabla en Heroku, vía sequelice-cli.

---
+++ migrations/20150811164918-create-comments.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      texto: {
+        type: Sequelize.STRING
+      },
+      QuizId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Quizzes,
+            key: id            
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Comments');
+  }
+};
